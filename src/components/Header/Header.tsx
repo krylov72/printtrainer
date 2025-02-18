5b623a0902afce95428619c9e9b19eee46937dd6
krylov72 <qobject1337@gmail.com>
2025-02-18 23:26:16 +0500

    printtrainer init project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header className="w-full flex justify-center gap-5 items-center">
+      
+      <h1 className="text-3xl font-bold">PRINT_TRAINER</h1>
+ 
+      
+
+
+      
+    </header>
+  );
+};
+
+export {Header}
